c9fb9728d4e3039011cc39d7676e0ead454faada
Hiroyuki Abe <plastico@ab-family.com>
2021-01-09 17:03:49 +0900

    add boyscout badge

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="hello">
-    <canvas id="canvas" width="500" height="500"></canvas>
-  </div>
+  <v-container bg fill-height grid-list-md text-xs-center>
+    <v-layout  align="center">
+          <v-container>
+                 <canvas 
+            id="canvas" width="500" height="500"></canvas>
+        </v-container>
+
+        </v-layout>
+  </v-container>
 </template>
 
 <script>
 import { fabric } from 'fabric-browseronly'
 
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  mounted() {
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+    mounted() {
     const canvas = new fabric.Canvas('canvas');
     
     const rect = new fabric.Rect({
@@ -21,27 +77,32 @@ export default {
       height: 100,
       hasControls:false
     });
+    new fabric.Image.fromURL('https://members.scouts.org.uk/images/content/badges/2015sc-as-ar.png', function(oImg) {
+        oImg.scale(0.25);
+        oImg.hasControls = false;
+        canvas.add(oImg);
+      });
+    new fabric.Image.fromURL('https://members.scouts.org.uk/images/content/badges/2015sc-cs-out.png', function(oImg) {
+      oImg.scale(0.25);
+      oImg.hasControls = false;
+      canvas.add(oImg);
+    });
+    new fabric.Image.fromURL('https://members.scouts.org.uk/images/content/badges/2015sc-cs-ski.png', function(oImg) {
+      oImg.scale(0.25);
+      oImg.hasControls = false;
+      canvas.add(oImg);
+    });
+    new fabric.Image.fromURL('https://members.scouts.org.uk/images/content/badges/2015sc-cs-adv.png', function(oImg) {
+      oImg.scale(0.25);
+      oImg.hasControls = false;
+      canvas.add(oImg);
+    });
     canvas.add(rect);
   }
-}
+  }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 #canvas {
   border: 4px solid;
 }
